d974efbef9af23b951386458a4e9222331502d7c
goran131 <goran.rosenberg@tomelilla.nu>
2026-03-16 10:57:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_I_Todo_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
